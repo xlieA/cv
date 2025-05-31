e12d6c586d2a74c99869642cbbd2af0eedb342b3
@natalie <natalie.adam@tum.de>
2025-05-31 23:38:22 +0200

    added responsivness for home section

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,15 @@ let mountain_back = document.getElementById("mountain_back");
 let mountain_moon = document.getElementById("mountain_moon");
 let mountain_middle = document.getElementById("mountain_middle");
 let mountain_front = document.getElementById("mountain_front");
-let text = document.getElementById("text");
+let greeting = document.getElementById("greeting");
 
 window.addEventListener('scroll', function() {
     var value = window.scrollY;
 
-    mountain_back.style.top = value * 0.5 + 'px';
+    mountain_back.style.top = -value * 0.5 + 'px';
     mountain_moon.style.left = -value * 0.9 + 'px';
+    mountain_moon.style.top = -value * 0.3 + 'px';
     mountain_middle.style.top = -value * 0.15 + 'px';
     mountain_front.style.top = value * 0.15 + 'px';
-    text.style.top = value * 1 + 'px';
+    greeting.style.top = value * 1 + 'px';
 })
