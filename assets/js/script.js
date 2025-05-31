57b5b1312b5485daa334e3243a17197ad9ab1529
@natalie <natalie.adam@tum.de>
2025-05-31 22:24:27 +0200

    finished home section

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,14 @@ let mountain_back = document.getElementById("mountain_back");
 let mountain_moon = document.getElementById("mountain_moon");
 let mountain_middle = document.getElementById("mountain_middle");
 let mountain_front = document.getElementById("mountain_front");
-let text = document.getElementById("document");
+let text = document.getElementById("text");
 
 window.addEventListener('scroll', function() {
     var value = window.scrollY;
 
     mountain_back.style.top = value * 0.5 + 'px';
-    mountain_moon.left = -value * 0.5 + 'px';
-    mountain_middle.top = -value * 0.15 + 'px';
-    mountain_front.top = value * 0.15 + 'px';
+    mountain_moon.style.left = -value * 0.9 + 'px';
+    mountain_middle.style.top = -value * 0.15 + 'px';
+    mountain_front.style.top = value * 0.15 + 'px';
     text.style.top = value * 1 + 'px';
 })
