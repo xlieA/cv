0293772b774159baeb2dde3b62c4e536d25f3529
@natalie <natalie.adam@tum.de>
2025-05-31 21:47:44 +0200

    added home section

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,19 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-xmark')
     navbar.classList.toggle('active');
 }
+
+let mountain_back = document.getElementById("mountain_back");
+let mountain_moon = document.getElementById("mountain_moon");
+let mountain_middle = document.getElementById("mountain_middle");
+let mountain_front = document.getElementById("mountain_front");
+let text = document.getElementById("document");
+
+window.addEventListener('scroll', function() {
+    var value = window.scrollY;
+
+    mountain_back.style.top = value * 0.5 + 'px';
+    mountain_moon.left = -value * 0.5 + 'px';
+    mountain_middle.top = -value * 0.15 + 'px';
+    mountain_front.top = value * 0.15 + 'px';
+    text.style.top = value * 1 + 'px';
+})
